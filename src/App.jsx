ea14800f7430edc8010232304274022d55360feb
maleeshacbandara <maleeshacbandara@gmail.com>
2024-11-21 13:27:03 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <div>
       <Navbar /> {/* Navbar at the top */}
-      <main className="space-y-16">
+      <main className="space-y-0"> {/* Removed space-y-16 to eliminate extra vertical spacing */}
         {/* Sections */}
         <div id="home" className="pt-20">
           <Home /> {/* Render Home component */}
@@ -22,13 +22,13 @@ const App = () => {
         <div id="about" className="pt-20">
           <About /> {/* Render About component */}
         </div>
-        <div id="menu" className="pt-20">
+        <div id="menu" className="pt-5 pb-2"> {/* Reduced padding-top */}
           <Menu /> {/* Render Menu component */}
         </div>
-        <div id="reviews" className="pt-20">
+        <div id="reviews" className="pt-0"> {/* Removed padding-top */}
           <Reviews /> {/* Render Review component */}
         </div>
-        <div id="footer" className="pt-20">
+        <div id="footer" className="pt-0">
           <Footer /> {/* Render Footer component */}
         </div>
       </main>
